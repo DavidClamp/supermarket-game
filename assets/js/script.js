065671a9a68e0348da1fe48af5f153e5752a9f9c
David Clamp <dclamp@yahoo.com>
2025-10-08 10:25:51 +0100

    Created a function to pick square instead of product image. future development

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,31 +15,53 @@ const productArray = [
     { name: "Product Eight", img: "assets/images/runner3.webp", value: 80 },
     { name: "Product Nine", img: "assets/images/runner4.webp", value: 90 },
 ]
-console.log(productArray);
-
-// // productArray.sort(() => 0.5 - Math.random());
 
 
-console.log(grid);
+//  productArray.sort(() => 0.5 - Math.random());
 
-function fillBoard() {
+
+//console.log(grid);
+// Fill the board with photos
+function fillBoardPhotos() {
     for (let i = 0; i < productArray.length; i++) {
         const product = document.createElement("img");
         product.setAttribute("src", productArray[i].img);
-        
-    product.setAttribute("data-id", i);
-    //    product.setAttribute("value", value);
+        console.log(productArray[i].value);
+       product.setAttribute("data-id", i);
        product.style.width= "100px";
        product.style.height = "150px";        
-     // product.addEventListener("click", pickProduct);
+       product.addEventListener("click", pickProduct);
         squares[i].appendChild(product);
     }   
 }
-fillBoard()
+fillBoardPhotos()
 
-// function pickProduct() {
-//     const productId = this.getAttribute("data-id");
-//     console.log(productArray[productId].name);
-//     this.setAttribute("src", productArray[productId].img);
-//    // this.style.border = "5px solid red";
+// Pick product via click
+function pickProduct() {
+    const productId = this.getAttribute("data-id");
+   this.style.border = "1px solid red";
+}
+// Pick square via click
+function pickSquare() {
+    const square = this.parentElement
+   this.style.border = "1px solid blue";
+}
+
+
+
+// fill board with question and responses
+// function fillBoardValue() {
+//     for (let j = 0; j < productArray.length; j++) {
+//         const productValue = document.querySelector(".square");
+//         productValue.innerText = "setAttribute("productArray[j].value")
+//         squares[j].appendChild(productValue);
+//     }   
 // }
+// fillBoardValue()
+
+createElement("div");
+productValue.classList.add("product-value");
+productValue.innerText = productArray[j].value;
+squares[j].appendChild(productValue);   
+
+
