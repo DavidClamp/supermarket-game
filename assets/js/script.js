ab1ff6a70187fbba7236c484031f8d6a69dcdfd6
David Clamp <dclamp@yahoo.com>
2025-10-09 08:53:08 +0100

    Tidied up code using Prettier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,46 @@
-  const grid = document.querySelector('.grid');
-  const squares = document.querySelectorAll('.square');
-         
-const resultDisplay = document.querySelector('#result');
+const grid = document.querySelector(".grid");
+const squares = document.querySelectorAll(".square");
+
+const resultDisplay = document.querySelector("#result");
 
 // Create Products array
 const productArray = [
-    { name: "Product One", img: "assets/images/athletes.webp", value: 10 },
-    { name: "Product Two", img: "assets/images/girls.webp",value: 20 },
-    { name: "Product Three", img: "assets/images/man.webp", value: 30 },
- { name: "Product Four", img: "assets/images/mud-run.webp", value: 40 },
-    { name: "Product Five", img: "assets/images/reasons-image.webp", value: 50 },
-    { name: "Product Six", img: "assets/images/runner1.webp"    , value: 60 },
-    { name: "Product Seven", img: "assets/images/runner2.webp" , value: 70 },
-    { name: "Product Eight", img: "assets/images/runner3.webp", value: 80 },
-    { name: "Product Nine", img: "assets/images/runner4.webp", value: 90 },
-]
-
+  { name: "Product One", img: "assets/images/athletes.webp", value: 10 },
+  { name: "Product Two", img: "assets/images/girls.webp", value: 20 },
+  { name: "Product Three", img: "assets/images/man.webp", value: 30 },
+  { name: "Product Four", img: "assets/images/mud-run.webp", value: 40 },
+  { name: "Product Five", img: "assets/images/reasons-image.webp", value: 50 },
+  { name: "Product Six", img: "assets/images/runner1.webp", value: 60 },
+  { name: "Product Seven", img: "assets/images/runner2.webp", value: 70 },
+  { name: "Product Eight", img: "assets/images/runner3.webp", value: 80 },
+  { name: "Product Nine", img: "assets/images/runner4.webp", value: 90 },
+];
 
 //  productArray.sort(() => 0.5 - Math.random());
-
 
 //console.log(grid);
 // Fill the board with photos
 function fillBoardPhotos() {
-    for (let i = 0; i < productArray.length; i++) {
-        const product = document.createElement("img");
-        product.setAttribute("src", productArray[i].img);
-        product.setAttribute("data-id", i);
-       product.style.width= "100px";
-       product.style.height = "100px";        
-       product.addEventListener("click", pickProductImage);
-        squares[i].appendChild(product);
+  for (let i = 0; i < productArray.length; i++) {
+    const product = document.createElement("img");
+    product.setAttribute("src", productArray[i].img);
+    product.setAttribute("data-id", i);
+    product.style.width = "100px";
+    product.style.height = "100px";
+    product.addEventListener("click", pickProductImage);
+    squares[i].appendChild(product);
 
-// Adjusted value by 10% up or down randomly and place to each square
+    // Adjusted value by 10% up or down randomly and place to each square
 
-       const adjValue = document.createElement("h5");
-       adjValue.innerText= " Is this price higher or lower than selling price?" + (productArray[i].value * (Math.random() < 0.5 ? 0.9 : 1.1)).toFixed(2);
-       console.log(adjValue);
-       squares[i].appendChild(adjValue);
-       
-       //is the adjusted value higher or lower than the actual value?
-       
+    const adjValue = document.createElement("h5");
+    adjValue.innerText =
+      " Is this price higher or lower than selling price?" +
+      (productArray[i].value * (Math.random() < 0.5 ? 0.9 : 1.1)).toFixed(2);
+    console.log(adjValue);
+    squares[i].appendChild(adjValue);
+
+    //is the adjusted value higher or lower than the actual value?
+
     //    let higherLower = adjValue > productArray[i].value ? "higher" : "lower";
     //    console.log(higherLower);
 
@@ -49,33 +49,32 @@ function fillBoardPhotos() {
     //    adjProductValue.innerText = " Your guess is " + higherLower + " than " + adjValue.toFixed(2);
     //     squares[i].appendChild(adjProductValue);
 
+    //place product value to each square
+    const productValue = document.createElement("h5");
+    productValue.innerText =
+      " Correct value = " + productArray[i].value.toFixed(2);
+    console.log(productValue);
+    squares[i].appendChild(productValue);
+  }
+}
 
-//place product value to each square
-       const productValue = document.createElement("h5");
-       productValue.innerText = " Correct value = " + (productArray[i].value).toFixed(2);
-       console.log(productValue);
-        squares[i].appendChild(productValue);
-    }   
-}       
-
-fillBoardPhotos()
+fillBoardPhotos();
 
 // Pick product via click
 function pickProductImage() {
-    const productId = this.getAttribute("data-id");
-   this.style.border = "1px solid red";
+  const productId = this.getAttribute("data-id");
+  this.style.border = "1px solid red";
 }
 // Function to add to each square a value that is either 10% higher or lower than the actual product value
 
- function placeValue() {
-// const value = this.getAttribute("data-id");
-//console.log(value);
-// Adjusted value by 10% up or down randomly
-// adjValue = productArray[1].value * (Math.random() < 0.5 ? 0.9 : 1.1);
-// console.log(adjValue);
+function placeValue() {
+  // const value = this.getAttribute("data-id");
+  //console.log(value);
+  // Adjusted value by 10% up or down randomly
+  // adjValue = productArray[1].value * (Math.random() < 0.5 ? 0.9 : 1.1);
+  // console.log(adjValue);
 }
 placeValue();
-
 
 // // Pick square via click
 // function pickSquare() {
@@ -84,9 +83,3 @@ placeValue();
 //     this.style.width = "100px";
 //     this.style.height= "150px";
 // }
-    
-
-
- 
-
-
